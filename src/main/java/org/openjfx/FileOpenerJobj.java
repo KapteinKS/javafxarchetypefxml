f3b3164bb23b667d4b3ccfb5b3c7be27a6be9cf1
matsovevada <55790581+matsovevada@users.noreply.github.com>
2020-02-11 17:55:10 +0100

    lagt til .jobj Fil klasser

---
+++ src/main/java/org/openjfx/FileOpenerJobj.java
@@ -0,0 +1,7 @@
+package org.openjfx;
+
+import java.io.InputStream;
+
+public class FileOpenerJobj implements FileOpener{
+
+}
---
+++ src/main/java/org/openjfx/FileSaverJobj.java
@@ -0,0 +1,7 @@
+package org.openjfx;
+
+import java.io.OutputStream;
+
+public class FileSaverJobj implements FileOpener {
+
+}
