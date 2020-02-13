dcd73ab1fb45c31c520cb006344d35417450c43d
matsovevada <55790581+matsovevada@users.noreply.github.com>
2020-02-13 11:54:01 +0100

    kscmdf

--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -1,12 +1,15 @@
 package org.openjfx;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -17,14 +20,16 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.util.converter.IntegerStringConverter;
 
 public class PrimaryController implements Initializable {
-    Register register = new Register();
     FileChooser fileChooser = new FileChooser();
     Stage mainStage = new Stage();
     File selectedFile;
-    private  DataCollection collection = new DataCollection();
+    Avvik avvik = new Avvik();
+    public IntegerStringConverter intStrConverter = new IntegerStringConverter();
+    private DataCollection collection = new DataCollection();
+    String fileType = "";
+
     @FXML
     private Button btsVisReg;
 
@@ -68,7 +73,7 @@ public class PrimaryController implements Initializable {
     private MenuItem lagreFilSom;
 
     @FXML
-    private TableView<?> tableView;
+    private TableView tableView;
 
     @FXML
     private TableColumn<DataModel, String> colNavn;
@@ -88,99 +93,146 @@ public class PrimaryController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         collection.attachTableView(tableView);
-        colAlder.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        colAlder.setCellFactory(TextFieldTableCell.forTableColumn(intStrConverter));
     }
 
     @FXML
     void saveRegistryAs(ActionEvent event) throws IOException {
         fileChooser.setTitle("Save to which file?");
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("Object Files", "*.jobj"));
         selectedFile = fileChooser.showSaveDialog(mainStage);
-        if(selectedFile != null) {
-            String paths = selectedFile.getAbsolutePath();
-            var path = Paths.get(paths);
-            FileSaverTxt.skrivTilFil(register.liste, path);
-            warninglbl.setText("Register lagret");
+        if(selectedFile != null){
+            String path = selectedFile.getAbsolutePath();
+            Path paths = Paths.get(path);
+            String stringPath = paths.toString();
+            String [] pathArray = stringPath.split("\\.");
+            fileType = pathArray[pathArray.length-1];
+            if(fileType.equals("txt")){
+                FileSaverTxt.skrivTilFil(collection.getList(), paths);
+            } else if(fileType.equals("jobj")){
+                FileSaverJobj.srivTilFil(collection.getList(), paths);
+            } else {
+                org.openjfx.Dialogs.showErrorDialog("Feil filformat valgt!");
+            }
         }
     }
 
     @FXML
     void saveRegistry(ActionEvent event) throws IOException {
-        fileChooser.setTitle("Save to which file?");
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
         if(selectedFile == null) {
+            fileChooser.setTitle("Save to which file?");
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                    new FileChooser.ExtensionFilter("Object Files", "*.jobj"));
             selectedFile = fileChooser.showSaveDialog(mainStage);
         }
-        if(selectedFile != null) {
-            String paths = selectedFile.getAbsolutePath();
-            var path = Paths.get(paths);
-            FileSaverTxt.skrivTilFil(register.liste, path);
-            warninglbl.setText("Register lagret");
+        if (selectedFile != null) {
+            String path = selectedFile.getAbsolutePath();
+            Path paths = Paths.get(path);
+            String stringPath = paths.toString();
+            String[] pathArray = stringPath.split("\\.");
+            fileType = pathArray[pathArray.length - 1];
+            if (fileType.equals("txt")) {
+                FileSaverTxt.skrivTilFil(collection.getList(), paths);
+            } else if (fileType.equals("jobj")) {
+                FileSaverJobj.srivTilFil(collection.getList(), paths);
+            } else {
+                org.openjfx.Dialogs.showErrorDialog("Feil filformat valgt!");
+            }
         }
+
     }
 
-    /*
     @FXML
-    void chooseFile(ActionEvent event) throws IOException {
+    void chooseFile(ActionEvent event) throws IOException, ClassNotFoundException {
         fileChooser.setTitle("Velg en fil som inneholder register");
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Object Files", "*.jobj"),
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"));
         selectedFile = fileChooser.showOpenDialog(mainStage);
         if(selectedFile != null) {
             String path = selectedFile.getAbsolutePath();
             Path paths = Paths.get(path);
-            List<Person> personer = FileOpenerTxt.lesFil(paths);
-            for (Person p : personer) {
-                register.leggTil(p);
-                DataModel dm = createDMfromPerson(p);
-                collection.addElement(dm);
+            String stringPath = paths.toString();
+            String [] pathArray = stringPath.split("\\.");
+            fileType = pathArray[pathArray.length-1];
+
+            if(fileType.equals("jobj")){
+                ObservableList<DataModel> list = FileOpenerJobj.lesFil(paths);
+
+                for(DataModel dm : list){
+                    collection.addElement(dm);
+                }
+            } else if(fileType.equals("txt")){
+                ArrayList<DataModel> list = FileOpenerTxt.lesFil(paths);
+
+                for(DataModel dm : list){
+                    collection.addElement(dm);
+                }
+            } else {
+                org.openjfx.Dialogs.showErrorDialog("Feil filformat valgt");
             }
-            warninglbl.setText("Person(er) lagt inn fra fil");
+
         }
     }
 
-     */
-
+/*
     @FXML
-    void regPers(ActionEvent event) {
-        if(!lblNavn.getText().isEmpty()) {
-            warninglbl.setText(register.registrerPerson(lblNavn.getText(), lblAlder.getText(),
-                    lblDD.getText(), lblMM.getText(), lblYYYY.getText(), txtEPost.getText(), txtTelefon.getText()));
+    void chooseFile(ActionEvent event) throws IOException, ClassNotFoundException {
+        fileChooser.setTitle("Velg en fil som inneholder register");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.jobj"));
+        selectedFile = fileChooser.showOpenDialog(mainStage);
+        if (selectedFile != null) {
+            String path = selectedFile.getAbsolutePath();
+            Path paths = Paths.get(path);
 
-            DataModel obj = createDataModelObjectFromGUI();
-
-            if(warninglbl.getText().equals("")){
-                if (obj != null) {
-                    resetTxtFields();
-                    collection.addElement(obj);
+            try(InputStream in = Files.newInputStream(paths);
+            ObjectInputStream oin = new ObjectInputStream(in))
+            {
+                List<DataModel> dataModelList = (List<DataModel>)oin.readObject();
+                for(DataModel d : dataModelList){
+                    collection.addElement(d);
                 }
             }
         }
     }
+ */
 
-    private DataModel createDMfromPerson(Person p){
-        String navn = p.getNavn();
-        int alder = p.getAlder();
-        Dato fDato = p.getfDato();
-        String tlf = p.getTelefon();
-        String epost = p.getePost();
-        return new DataModel(navn, alder, fDato, tlf, epost);
+    @FXML
+    void regPers(ActionEvent event) {
+        if(!lblNavn.getText().isEmpty()) {
+            DataModel obj = createDataModelObjectFromGUI();
+            if (obj != null) {
+                resetTxtFields();
+                collection.addElement(obj);
+            }
+        }
     }
 
     private DataModel createDataModelObjectFromGUI() {
-        String navn = lblNavn.getText();
-        String ePost = txtEPost.getText();
-        String tlf = txtTelefon.getText();
-
         try {
-            int alder = Integer.parseInt(lblAlder.getText());
-            int dag = Integer.parseInt(lblDD.getText());
-            int måned = Integer.parseInt(lblMM.getText());
-            int år = Integer.parseInt(lblYYYY.getText());
+            String navn = avvik.sjekkNavn(lblNavn.getText());
+            int alder = avvik.alder(Integer.parseInt(lblAlder.getText()));
+            int dag = avvik.dag(Integer.parseInt(lblDD.getText()));
+            int måned = avvik.måned(Integer.parseInt(lblMM.getText()));
+            int år = avvik.år(Integer.parseInt(lblYYYY.getText()));
+            String tlf = avvik.sjekkTelefon(txtTelefon.getText());
+            String ePost = avvik.sjekkEpost(txtEPost.getText());
+
             Dato fDato = new Dato(dag, måned, år);
             return new DataModel(navn, alder, fDato, tlf, ePost);
-        } catch (IllegalArgumentException e) {
+        } catch (InvalidNameException e) {
+            warninglbl.setText(e.getMessage());
+        } catch (NumberFormatException n){
+            warninglbl.setText("Kun heltall kan skrives inn i alder og datofelt");
+            return null;
+        } catch (InvalidAgeException | InvalidDateException | InvalidEmailException | InvalidPhoneException e){
+            warninglbl.setText(e.getMessage());
             return null;
         }
+        return null;
     }
 
     private void resetTxtFields() {
@@ -191,31 +243,30 @@ public class PrimaryController implements Initializable {
         lblYYYY.setText("");
         txtEPost.setText("");
         txtTelefon.setText("");
+        warninglbl.setText("");
     }
 
     public void nameDataEdited(TableColumn.CellEditEvent<DataModel, String> event) {
         event.getRowValue().setNavn(event.getNewValue());
-        Person p = register.getPerson(colTlf.getText());
-        if(p != null) {
-            p.setNavn(event.getNewValue());
-        }
     }
 
     public void phoneDataEdited(TableColumn.CellEditEvent<DataModel, String> event) {
         event.getRowValue().setTlf(event.getNewValue());
-        Person p = register.getPerson(event.getOldValue());
-        p.setePost(event.getNewValue());
     }
 
     public void emailDataEdited(TableColumn.CellEditEvent<DataModel, String> event) {
         event.getRowValue().setePost(event.getNewValue());
-        Person p = register.getPerson(colTlf.getText());
-        p.setePost(event.getNewValue());
-
     }
 
     public void alderDataEdited(TableColumn.CellEditEvent<DataModel, Integer> event) {
-        event.getRowValue().setAlder(event.getNewValue());
-        Person p = register.getPerson(colTlf.getText());
+        if(intStrConverter.wasSuccessful()) {
+            try {
+                event.getRowValue().setAlder(event.getNewValue());
+            } catch (IllegalArgumentException e){
+                Dialogs.showErrorDialog("Du kan ikke taste inn et negativt tall");
+            }
+        }
+
+        tableView.refresh();
     }
 }
